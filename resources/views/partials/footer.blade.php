0d4ff4972d8f9c14a276edc7a61087b88c9ca066
MorenaMastromarino <morena.mastromarino98@gmail.com>
2022-01-21 18:26:29 +0100

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,77 @@
 <footer>
-  <h1>footer</h1>
+  <div class="footer-top">
+    <div class="container">
+      <div class="footer-links">
+
+        <div class="column">
+          <h3>dc comics</h3>
+          <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li> 
+          </ul>
+
+          <h3>shop</h3>
+          <ul>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>      
+          </ul>
+        </div>
+
+        <div class="column">
+          <h3>dc</h3>
+          <ul>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+
+        <div class="column">
+        <h3>sites</h3>
+        <ul>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+        </ul>
+        </div>
+        
+      </div>
+
+      <div class="footer-logo">
+      <img src="{{ asset('img/dc-logo-bg.png') }}" alt="dc logo">
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-bottom">
+    <div class="container">
+
+      <div class="button">sign-up now!</div>
+
+      <div class="social-icons">
+        <a href="#">follow us</a>
+        <a href="#"><img src="{{ asset('img/footer-facebook.png') }}" alt="facebook logo"></a>
+        <a href="#"><img src="{{ asset('img/footer-twitter.png') }}" alt="twitter logo"></a>
+        <a href="#"><img src="{{ asset('img/footer-youtube.png') }}" alt="youtube logo"></a>
+        <a href="#"><img src="{{ asset('img/footer-pinterest.png') }}" alt="pinterest logo"></a>
+        <a href="#"><img src="{{ asset('img/footer-periscope.png') }}" alt="periscope logo"></a>        
+      </div>
+
+    </div>
+  </div>
 </footer>
